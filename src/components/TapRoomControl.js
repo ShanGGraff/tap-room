63015a9917440384c577e1cdc53ead37c559a4dd
Shane Graff <copellius@gmail.com>
2022-01-21 13:30:20 -0800

    Add scaffolding code to TapRoomControl and NewKegForm components

---
+++ src/components/TapRoomControl.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NewTicketForm from './NewKegForm';
+import TicketList from './KegList';
+
+class TapRoomControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default TapRoomControl;
